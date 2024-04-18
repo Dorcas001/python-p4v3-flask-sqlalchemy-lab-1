5ec2553da2fb24b0ca53c8d04af817ded1ce3e32
karimi <karimidorcas1@gmail.com>
2024-04-18 15:32:44 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -21,6 +21,29 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id =id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def quake_by_id(magnitude):
+    quake = []
+    for earth in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quake.append(earth.to_dict())
+    body = {'count': len(quake),
+            'quakes': quake }
+    return make_response(body, 200)
+
+
 
 
 if __name__ == '__main__':
